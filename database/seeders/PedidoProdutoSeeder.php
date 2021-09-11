7dd31b19b755f8c14cafee6c164beec8c5145b61
LucasCassianoCSI <80645551+LucasCassianoCSI@users.noreply.github.com>
2021-09-10 21:31:44 -0300

    up

--- database/seeders/PedidoProdutoSeeder.php
+++ database/seeders/PedidoProdutoSeeder.php
@@ -16,8 +16,35 @@ class PedidoProdutoSeeder extends Seeder
     public function run()
     {
         PedidoProduto::create([
-            'produto_id' => '2',
-
+        'produto_id' => '1',
+        'pedido_id'  => '1',
+        'quantidade' => '2',
+        'valor'      => '9.90',
             ]);
+           
+        PedidoProduto::create([
+        'produto_id' => '2',
+        'pedido_id'  => '1',
+        'quantidade' => '3',
+        'valor'      => '5.90',
+                ]);   
+
+        PedidoProduto::create([
+        'produto_id' => '4',
+        'pedido_id'  => '1',
+        'quantidade' => '1',
+        'valor'      => '15.90',
+                    ]);  
+                    
+        PedidoProduto::create([
+        'produto_id' => '2',
+        'pedido_id'  => '2',
+        'quantidade' => '2',
+        'valor'      => '11.90',
+                        ]);
+
+                        
+
+
     }
 }
