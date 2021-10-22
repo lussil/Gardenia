4a2c764a383de9f69e413aa0227db783d25290f1
Guilherme Lino <guilhermedjango15@gmail.com>
2021-10-22 20:04:39 -0300

    up-quemsomos

--- resources/views/doceriagardenia/quemSomos.blade.php
+++ resources/views/doceriagardenia/quemSomos.blade.php
@@ -44,11 +44,96 @@
       </header>
     <br>
     <h1 style="text-align: center;">Quem Somos</h1>
-    <br>
-    <section class="">
-      <div>
-        
-      </div>
+    <section class="intro-quemsomos">
+      <img
+        src="https://static.zerochan.net/Levi.Ackerman.full.2271834.jpg"
+        alt=""
+        srcset=""
+      />
+    </section>
+    <br />
+
+    <section class="QS-containerGeral">
+      <ul>
+        <li class="QS-containerConteudo">
+            <article>
+            <div class="infromacoes">
+              <h2>Gardênia</h2>
+              <p>
+                A Doceria Gardênia é uma micro-empresa que foi criada em outubro
+                de 2019 e seu nome oficial foi dado em fevereiro de 2020. Em seu
+                nascimento tiveram participação importante de seus amigos e
+                parentes, que passaram a incentivar na produção e início de um
+                próprio negócio. A partir de uma pesquisa em sua comunidade,
+                observou-se que seu produto tinha potencial para ganhar lugar no
+                mercado. Seu objetivo foi verificar a qualidade de seus produtos
+                primários (Donuts e Bolos) e após os positivos resultados, o
+                produto que obteve mais destaque foi o donuts. Tornando-se o
+                "Queridinho da casa".
+              </p>
+            </div>
+            <img
+              src="https://static1.purebreak.com.br/articles/0/49/71/0/@/216582-gravacoes-de-deadpool-2-devem-comecar-200x200-3.jpg"
+              alt="Donuts"
+            />
+          </article>
+        </li>
+        <br />
+
+        <li class="QS-containerConteudo">
+            <article>
+          <img
+            src="https://static1.purebreak.com.br/articles/0/49/71/0/@/216582-gravacoes-de-deadpool-2-devem-comecar-200x200-3.jpg"
+            alt="Donuts"
+          />
+          <div class="infromacoes">
+            <h2>Missão</h2>
+            <p>
+              Para entregar um produto nesses padrões, uso materiais de boa
+              qualidade, faço uma boa higiene no local de trabalho e sempre faço
+              pesquisa de preço p manter o custo-benefício dos clientes
+            </p>
+          </div>
+          <article/>
+        </li>
+        <br />
+
+        <li class="QS-containerConteudo">
+            <article>
+          <div class="infromacoes">
+            <h2>Visão</h2>
+            <p>
+              Sucesso pra mim não é necessariamente ficar rica, vejo isso como a
+              consequência de um bom trabalho. Sucesso para mim é fazer algo e
+              ter pessoas satisfeitas com isso. Ter clientes satisfeitos não tem
+              preço, é ótimo receber bons feedbacks e ver um cliente fidelizar.
+            </p>
+          </div>
+          <img
+            src="https://static1.purebreak.com.br/articles/0/49/71/0/@/216582-gravacoes-de-deadpool-2-devem-comecar-200x200-3.jpg"
+            alt="Donuts"
+          />
+          <article/>
+        </li>
+        <br />
+
+        <li class="QS-containerConteudo">
+            <article>
+          <img
+            src="https://static1.purebreak.com.br/articles/0/49/71/0/@/216582-gravacoes-de-deadpool-2-devem-comecar-200x200-3.jpg"
+            alt="Donuts"
+          />
+          <div class="infromacoes">
+            <h2>Valores</h2>
+            <p>
+              Um produto artesanal de boa qualidade, com cuidado nos mínimos
+              detalhes, ou seja, o produto é bonito visualmente, gostoso e com
+              um ótimo custo-benefício para o cliente, sempre.
+            </p>
+          </div>
+          <article/>
+        </li>
+      </ul>
     </section>
     <footer>
         <section class="container">
